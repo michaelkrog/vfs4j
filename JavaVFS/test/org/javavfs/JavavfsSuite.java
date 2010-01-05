7b4ed010e617f3ce50c834d123d6db3006f9eb86
michael.krog <michael.krog@9371596f-c54b-0410-9604-f9076ccabf6d>
2010-01-05 06:54:04 +0000

    Removed QT-related filesystem classes as Jambi is deprecated.

--- JavaVFS/test/org/javavfs/JavavfsSuite.java
+++ JavaVFS/test/org/javavfs/JavavfsSuite.java
@@ -14,8 +14,7 @@ import org.junit.runners.Suite;
  */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({org.javavfs.NativeFSFileTest.class,org.javavfs.NativeFSDirectoryTest.class,org.javavfs.NativeFSUseTest.class,
-                    org.javavfs.CIFSFileTest.class,org.javavfs.CIFSDirectoryTest.class,org.javavfs.CIFSUseTest.class,
-                    org.javavfs.QtFileTest.class,org.javavfs.QtDirectoryTest.class,org.javavfs.QtUseTest.class})
+                    org.javavfs.CIFSFileTest.class,org.javavfs.CIFSDirectoryTest.class,org.javavfs.CIFSUseTest.class})
 public class JavavfsSuite {
 
     public JavavfsSuite() {
